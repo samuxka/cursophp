cedb171a4d34cb8e92321ac97c2ffbe9340085a1
Samuel <109113326+samuxka@users.noreply.github.com>
2024-02-06 17:56:20 -0300

    open

---
+++ ex000/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo "Hello World \u{1F642}"
+        ?>
+    </h1>
+    <p>Vamos tentar nos livrar da maldição</p>
+</body>
+</html>
