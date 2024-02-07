1283ca98f80e33282ca9965c8a596bd0d2abd434
Samuel <109113326+samuxka@users.noreply.github.com>
2024-02-07 15:48:11 -0300

    php

---
+++ ex003/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Teste de tipos primitivos</h1>
+    <?php
+
+    // 0x = hexadecimal, 0b = binario e 0 = octal
+    // $num = 010;
+    // echo "o valor da variavel é $num";
+
+    // $v = 3e2;
+    // var_dump($v);
+
+    //$num = (integer) 3e2; // 3 x 10(2) coerção
+    //echo "o valor e $num";
+    //var_dump($num);
+
+    // $num = (int) "950";
+    // var_dump($num);
+
+    // $casado = false;
+    // //var_dump($num)
+    // print "O valor de casado é $casado";
+
+    // $vet = [6, 2, 9, 3, 5];
+    //     // echo "o vetor é $vet";
+    // var_dump($vet);
+
+    // class Pessoa{
+    //     private string $nome;
+    // }
+
+    // $p = new Pessoa;
+    // var_dump($p)
+    ?>
+</body>
+
+</html>
