bfbc3e83d6f3b5b80e7a1df01eb6d962cc71b170
Samuel <109113326+samuxka@users.noreply.github.com>
2024-02-13 18:20:28 -0300

    miuui

--- desafios/ex004/ex.php
+++ desafios/ex004/ex.php
@@ -10,14 +10,28 @@
 
 <body>
     <header>
-        <h1>Numero aleatorio</h1>
+        <h1>Conversor de moeda</h1>
     </header>
     <main>
         <?php
-            $numeroAleatorio = rand(1, 100);
-            echo "o numero gerado foi $numeroAleatorio";
-        ?>
-        <input type="submit" value="Gerar outro" reload>
+
+            $inicio = date("m-d-Y", strtotime("-7 days"));
+            $fim = date("m-d-Y");
+
+            $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''. $inicio .'\'&@dataFinalCotacao=\''. $fim .'\'&$top=100&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+
+            $dados = json_decode(file_get_contents($url), true);
+
+            $cotacao = $dados["value"][0]["cotacaoCompra"];
+
+            $real = $_GET["number"];
+            $dolar = $real / $cotacao;
+
+            $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+
+            echo "Seus " . numfmt_format_currency($padrao, $real, "BRL") . " Em dolar valem " . numfmt_format_currency($padrao, $dolar, "USD");
+        ?><br>
+        <a href="javascript:history.go(-1)">voltar</a>
     </main>
 </body>
 
