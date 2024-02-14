a6c10687e3670b295490f43f0ccda562e56a0a65
Samuel <109113326+samuxka@users.noreply.github.com>
2024-02-14 19:31:29 -0300

    baradara

--- desafios/ex005/ex.php
+++ desafios/ex005/ex.php
@@ -9,16 +9,24 @@
 </head>
 
 <body>
-    <header>
-        <h1>Numero aleatorio</h1>
-    </header>
     <main>
+        <h1>analisador de Número</h1>
         <?php
-            $numeroAleatorio = rand(1, 100);
-            echo "o numero gerado foi $numeroAleatorio";
-        ?>
-        <input type="submit" value="Gerar outro" reload>
+        $num = $_POST["number"] ?? 0;
+
+        echo "<p>Analisando o número <strong>" . number_format($num, 3, ",", ".") . "</strong> informado pelo usuário:</p>";
+
+        $int = (int) $num;
+        $fra = $num - $int;
+
+        echo "<ul>
+                    <li>A parte inteira do seu número é <strong>" . number_format($int, 0, ",", ".") . "</strong></li><br>
+                    <li>A parte fracionada do seu número é <strong>" . number_format($fra, 3, ".") . "</strong></li>
+                </ul>";
+        ?><br>
+        <button onclick="javascript:history.go(-1)">voltar</button>
     </main>
+</body>
 </body>
 
 </html>
