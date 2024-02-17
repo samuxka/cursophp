0e891a3f63e8657bbe82faf6ce49601670dab8f9
Samuel <109113326+samuxka@users.noreply.github.com>
2024-02-16 22:14:19 -0300

    finalmente

--- desafios/ex012/ex.php
+++ desafios/ex012/ex.php
@@ -9,7 +9,36 @@
 </head>
 
 <body>
-    
+    <?php 
+        $tempo = $_GET['tempo'] ?? 0;
+
+        $anos = $tempo / (60 * 24 * 365.25);
+        $meses = $tempo / (60 * 24 * 30.44);
+        $semanas = $tempo / (60 * 24 * 7);
+        $dias = $tempo / (60 * 24);
+        $horas = $tempo / 60;
+        $segundos = $tempo * 60;
+        $milisegundos = $tempo / 60000;
+    ?>
+
+    <main>
+        <h1>Calculo de tempo</h1>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+            <label for="time">Digite o tempo que quer calcular (em minutos)</label>    
+            <input type="number" name="tempo" value="<?=$tempo?>" required>
+            <input type="submit" value="Calcular">
+    </form>
+    </main>
+    <section>
+        <h2>Aqui esta o resultado</h2>
+        <p>O <?=$tempo?> em anos é <?=$anos?> anos</p>
+        <p>O <?=$tempo?> em meses é <?= $meses?> meses</p>
+        <p>O <?=$tempo?> em semanas é <?=$semanas?> semanas</p>
+        <p>O <?=$tempo?> em dias é <?=$dias?> dias</p>
+        <p>O <?=$tempo?> em horas é <?=$horas?> horas</p>
+        <p>O <?=$tempo?> em segundos é <?=$segundos?> segundos</p>
+        <p>O <?=$tempo?> em milisegundos é <?= $milisegundos?> milisegundos</p>
+    </section>
 </body>
 
 </html>
--- desafios/ex013/ex.php
+++ desafios/ex013/ex.php
@@ -9,35 +9,53 @@
 </head>
 
 <body>
-    <?php 
-        $tempo = $_GET['tempo'] ?? 0;
+    <?php
+    $saque = $_GET['saque'] ?? 0;
 
-        $anos = $tempo / (60 * 24 * 365.25);
-        $meses = $tempo / (60 * 24 * 30.44);
-        $semanas = $tempo / (60 * 24 * 7);
-        $dias = $tempo / (60 * 24);
-        $horas = $tempo / 60;
-        $segundos = $tempo * 60;
-        $milisegundos = $tempo / 60000;
+    $resto = $saque;
+    $tot100 = floor($resto / 100);
+    $resto %= 100;
+
+    $tot50 = floor($resto / 50);
+    $resto %= 50;
+
+    $tot20 = floor($resto / 20);
+    $resto %= 20;
+
+    $tot10 = floor($resto / 10);
+    $resto %= 10;
+
+    $tot5 = floor($resto / 5);
+    $resto %= 5;
+
+    $tot2 = floor($resto / 2);
+    $resto %= 2;
+
+    $tot1 = floor($resto / 1);
+    $resto %= 1;
     ?>
 
     <main>
-        <h1>Calculo de tempo</h1>
-        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
-            <label for="time">Digite o tempo que quer calcular (em minutos)</label>    
-            <input type="number" name="tempo" value="<?=$tempo?>" required>
-            <input type="submit" value="Calcular">
-    </form>
+        <h1>Caixa Eletrônico</h1>
+        <form action="<?= $_SERVER['PHP_SELF'] ?>" method="get">
+            <label for="saque">Qual valor você deseja sacar? (R$)<sup>*</sup></label>
+            <input type="number" name="saque" id="saque" value="<?= $saque ?>" required>
+            <p style="font-size: 0.7em;"><sup>*</sup>Notas disponiveis: R$100, R$50, R$10 e R$5</p>
+            <input type="submit" value="sacar">
+        </form>
     </main>
     <section>
-        <h2>Aqui esta o resultado</h2>
-        <p>O <?=$tempo?> em anos é <?=$anos?> anos</p>
-        <p>O <?=$tempo?> em meses é <?= $meses?> meses</p>
-        <p>O <?=$tempo?> em semanas é <?=$semanas?> semanas</p>
-        <p>O <?=$tempo?> em dias é <?=$dias?> dias</p>
-        <p>O <?=$tempo?> em horas é <?=$horas?> horas</p>
-        <p>O <?=$tempo?> em segundos é <?=$segundos?> segundos</p>
-        <p>O <?=$tempo?> em milisegundos é <?= $milisegundos?> milisegundos</p>
+        <h2>Saque de R$[??] realizado</h2>
+        <p>O caixa Eletrônico vai te entregar as seguintes notas</p>
+        <ul>
+            <li><img src="img/100-reais.jpg"> x<?= $tot100 ?></li>
+            <li><img src="img/50-reais.jpg"> x<?= $tot50 ?></li>
+            <li><img src="img/20-reais.jpg"> x<?= $tot20 ?></li>
+            <li><img src="img/10-reais.jpg"> x<?= $tot10 ?></li>
+            <li><img src="img/5-reais.jpg"> x<?= $tot5 ?></li>
+            <li><img src="img/2-reais.jpg"> x<?= $tot2 ?></li>
+            <li><img src="img/1-real.jpg"> x<?= $tot1 ?></li>
+        </ul>
     </section>
 </body>
 
